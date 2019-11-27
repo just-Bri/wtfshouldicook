aef03b7560189919339f61dc382043b9fc82355a
reifnotreef <bridgerrhammond@gmail.com>
2019-11-27 10:00:16 -0600

    working on styling. found a problem when posting recipe ingredients

--- src/Recipe/Recipe.js
+++ src/Recipe/Recipe.js
@@ -36,34 +36,40 @@ export default class Recipe extends Component {
         {this.state.recipeDetails.map((item, i) => {
           return (
             <ul key={i} className="specific-details">
-              <li>Name: {item.name}</li>
-              <li>Prep time: {item.prep_time}</li>
-              <li>Cook time: {item.cook_time}</li>
-              <li>Cuisine: {item.cuisine}</li>
-              <li>complex: {item.complex === false ? "nope" : "yep"}</li>
+              <li className="recipe-name">name: {item.name}</li>
+              <li className="recipe-cuisine">cuisine: {item.cuisine}</li>
+              <li className="recipe-complex">
+                complex: {item.complex === false ? "nope" : "yep"}
+              </li>
+              <li className="recipe-prep-time">prep time: {item.prep_time}</li>
+              <li className="recipe-cook-time">cook time: {item.cook_time}</li>
             </ul>
           );
         })}
-        <h3 className="specific-ing-header">Ingredients</h3>
-        <ul className="specific-ing-list">
-          {this.state.recipeIngredients.map((item, i) => {
-            return (
-              <li key={i} className="specific-ing-item">
-                {i + 1}: {item.name}, {item.amount}
-              </li>
-            );
-          })}
-        </ul>
-        <h3 className="specific-instrc-header">Instructions</h3>
-        <ul className="specific-instrc-list">
-          {this.state.recipeInstructions.map((item, i) => {
-            return (
-              <li key={i} className="specific-instrc-item">
-                Step {i + 1}: {item.instructions}
-              </li>
-            );
-          })}
-        </ul>
+        <section className="ing-container">
+          <h3 className="specific-ing-header">ingredients</h3>
+          <ul className="specific-ing-list">
+            {this.state.recipeIngredients.map((item, i) => {
+              return (
+                <li key={i} className="specific-ing-item">
+                  {i + 1}: {item.name}, {item.amount}
+                </li>
+              );
+            })}
+          </ul>
+        </section>
+        <section className="instr-container">
+          <h3 className="specific-instrc-header">instructions</h3>
+          <ul className="specific-instrc-list">
+            {this.state.recipeInstructions.map((item, i) => {
+              return (
+                <li key={i} className="specific-instrc-item">
+                  Step {i + 1}: {item.instructions}
+                </li>
+              );
+            })}
+          </ul>
+        </section>
       </section>
     ) : this.state.done === "yes" ? (
       <Uhoh />
